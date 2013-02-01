9a55555d5af277d16bd01e6db5c03e0fe605cf56
Deaboy <danieleandnus@gmail.com>
2013-02-01 10:00:47 -0700

    Updated to latest version of Bukkit Added command to list player data files.

--- src/com/deaboy/invmaster/InvMasterCommands.java
+++ src/com/deaboy/invmaster/InvMasterCommands.java
@@ -1,6 +1,11 @@
 package com.deaboy.invmaster;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -15,6 +20,7 @@ public class InvMasterCommands implements CommandExecutor
 		Bukkit.getPluginCommand("inventory").setExecutor(this);
 		Bukkit.getPluginCommand("enderchest").setExecutor(this);
 		Bukkit.getPluginCommand("armor").setExecutor(this);
+		Bukkit.getPluginCommand("listplayers").setExecutor(this);
 	}
 	
 	@Override
@@ -130,6 +136,72 @@ public class InvMasterCommands implements CommandExecutor
 			}
 			
 			((Player) sender).openInventory(inv);
+			
+			return true;
+		}
+		
+		if (cmd.getName().equalsIgnoreCase("listplayers"))
+		{
+			if (!sender.isOp())
+			{
+				sender.sendMessage(ChatColor.RED + "You don't have permission to do that!");
+				return true;
+			}
+			
+			int page;
+			int perpage = 8;
+			List<String> names = new ArrayList<String>();
+			
+			if (args.length >= 1)
+			{
+				try
+				{
+					page = Integer.parseInt(args[1]);
+					if (page < 1)
+						page = 1;
+				}
+				catch (NumberFormatException e)
+				{
+					page = 1;
+				}
+			}
+			else
+			{
+				page = 1;
+			}
+			
+			page--;
+			
+			File folder = new File(Bukkit.getWorlds().get(0).getWorldFolder() + "/players");
+			if (!folder.isDirectory())
+			{
+				sender.sendMessage("There was a problem listing the files!");
+				return true;
+			}
+			for (File file : folder.listFiles())
+			{
+				if (file.getName().endsWith(".dat"))
+					names.add(file.getName().substring(0, file.getName().length()-4));
+			}
+			
+			if (names.size() == 0)
+			{
+				sender.sendMessage("It appears that no player files exist in the default world's \"players\" directory...");
+				return true;
+			}
+			
+			
+			if (page*perpage > names.size())
+				page = names.size() / perpage;
+			
+			sender.sendMessage("Currently saved player data files (" + (page+1) + "/" + ((names.size()/perpage)+1) + ")");
+			
+			names = names.subList(page*perpage, (page+1)*perpage > names.size() ? names.size() : (page+1)*perpage);
+			
+			for (String name : names)
+			{
+				sender.sendMessage("  " + name);
+			}
 			
 			return true;
 		}
